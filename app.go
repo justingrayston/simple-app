45e79e1924db8ab74ffcadbde51de0b65dc891fd
Justin Grayston <jgrayston@google.com>
2018-12-18 09:14:09 +0000

    Revert to original

--- app.go
+++ app.go
@@ -14,7 +14,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if msg == "" {
 		msg = ":( SIMPLE_MSG variable not defined"
 	}
-	fmt.Fprintf(w, "<h1>%s</h1><p>boo</boo>", msg)
+	fmt.Fprintf(w, "<h1>%s</h1>", msg)
 }
 
 func main() {
